9c78d37c6625b1f6135f17fdfaa2e827a6268c4b
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2020-05-28 22:49:46 +0000

    Test for random letter

--- CTA_1.py
+++ CTA_1.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import random
+
+# To do
+# Create a list of letters for coffee
+tokens = ["C", "O", "F", "E"]
+already_got = []
+
+# Pick random letters from a list (looped for testing purposes)
+keep_going = ""
+while  keep_going == "":
+  user = input("Enter to continue: ")
+  com = random.choice(tokens)
+  already_got.append(com)
+  print(already_got)
+  keep_going = input("Continue? ")
+print("End")
